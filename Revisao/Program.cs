7aa80425d48aee4896f43444f4f856ac2b546054
Victor Augusto <victor.silva452@outlook.com>
2019-03-30 10:21:15 -0300

    Initial...

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace Revisao
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
 }
